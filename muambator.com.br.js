40b75002f369a2aa6416d7dbb1cd1e0ac7a9b997
Felipe Coury <felipe.coury@gmail.com>
2018-11-26 12:53:01 -0200

    feat: added Muambator link to pending packages

---
+++ muambator.com.br.js
@@ -0,0 +1,20 @@
+const logoEl = document.getElementsByTagName('img')[0];
+const linkEl = logoEl.parentElement;
+linkEl.setAttribute('href', '/pacotes/pendentes/');
+
+const el = document.getElementsByClassName('dropdown-toggle')[0];
+const parentEl = el.parentElement;
+
+const ulEl = parentEl.getElementsByTagName('ul')[0];
+console.log(parentEl)
+console.log(ulEl)
+
+const newEl = document.createElement('a');
+newEl.setAttribute('class', 'menu-item');
+newEl.setAttribute('href', '/pacotes/pendentes/');
+newEl.setAttribute('title', 'Pendentes');
+newEl.textContent = 'Pendentes';
+
+parentEl.removeChild(ulEl);
+parentEl.removeChild(el);
+parentEl.appendChild(newEl);
