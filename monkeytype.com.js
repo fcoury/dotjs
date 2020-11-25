5dd4e86447a1983c1cabfc805044f9ac29f3d13c
Felipe Coury <felipe.coury@gmail.com>
2020-11-25 12:05:42 -0300

    feat: added monkeytype wpm integration to twitch bot

---
+++ monkeytype.com.js
@@ -0,0 +1,17 @@
+(function() {
+  function checkForWpm() {
+    const wpmEl = document.querySelectorAll('.wpm > [data-balloon-pos="up"]');
+    if (wpmEl) {
+      const wpm = wpmEl[0].textContent;
+      if (wpm.indexOf('-') === -1) {
+        console.log('wpm', wpm);
+        fetch(`http://localhost:3000/finishWpm?wpm=${wpm}`);
+        return;
+      }
+    }
+    setTimeout(checkForWpm, 200);
+  }
+
+  console.log('checking for wpm...');
+  checkForWpm();
+})();
